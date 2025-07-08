7ea99c202763c82209b37cd121cf8a4f4414d519
Jair Meza <jair3202@gmail.com>
2025-07-08 11:14:24 -0400

    cleanup

--- src/api/queryExecutor.ts
+++ src/api/queryExecutor.ts
@@ -1,4 +1,4 @@
-import { AllRequests } from "./dataFetchingUtils";
+import { AllRequests } from "./requestBuilder";
 
 import { LazyQueryExecFunction, OperationVariables } from "@apollo/client";
 
@@ -13,45 +13,32 @@ export interface QueryExecutors {
 /**
  * Execute all queries concurrently based on built requests
  */
-export async function executeAllQueries(
-  requests: AllRequests,
-  executors: QueryExecutors
-): Promise<void> {
+export async function executeAllQueries(requests: AllRequests, executors: QueryExecutors): Promise<void> {
   const promises: Promise<unknown>[] = [];
 
   // Execute BigWig/BigBed queries
   if (requests.bigRequests.length > 0) {
-    promises.push(
-      executors.fetchBigData({ variables: { bigRequests: requests.bigRequests } })
-    );
+    promises.push(executors.fetchBigData({ variables: { bigRequests: requests.bigRequests } }));
   }
 
   // Execute transcript query
   if (requests.transcriptRequest) {
-    promises.push(
-      executors.fetchGene({ variables: requests.transcriptRequest })
-    );
+    promises.push(executors.fetchGene({ variables: requests.transcriptRequest }));
   }
 
   // Execute motif query
   if (requests.motifRequest) {
-    promises.push(
-      executors.fetchMotif({ variables: requests.motifRequest })
-    );
+    promises.push(executors.fetchMotif({ variables: requests.motifRequest }));
   }
 
   // Execute importance queries
   if (requests.importanceRequests.length > 0) {
-    promises.push(
-      executors.fetchImportance({ variables: { bigRequests: requests.importanceRequests } })
-    );
+    promises.push(executors.fetchImportance({ variables: { bigRequests: requests.importanceRequests } }));
   }
 
   // Execute LD query
   if (requests.ldRequest) {
-    promises.push(
-      executors.fetchSnps({ variables: requests.ldRequest })
-    );
+    promises.push(executors.fetchSnps({ variables: requests.ldRequest }));
   }
 
   // Execute all queries concurrently
