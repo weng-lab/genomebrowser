49af82a5b5b62d5b533cccf038be5968d227c8e7
Jair Meza <jair3202@gmail.com>
2025-12-27 18:34:52 -0500

    export rowbyid

--- packages/ui/src/lib.ts
+++ packages/ui/src/lib.ts
@@ -6,3 +6,9 @@ import {
   type SelectionStoreInstance,
 } from "./TrackSelect/store.ts";
 export { createSelectionStore, SelectionStoreInstance };
+
+import { rowById } from "./TrackSelect/consts.ts";
+export { rowById };
+
+import type { RowInfo } from "./TrackSelect/types.ts";
+export { RowInfo };
