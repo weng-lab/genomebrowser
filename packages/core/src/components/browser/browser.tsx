4c5bda7235e6628036d05dc7f96ec6e2a7b40172
Jair Meza <jair3202@gmail.com>
2026-03-09 19:32:37 -0400

    inline browser shell implementation

--- packages/core/src/components/browser/browser.tsx
+++ packages/core/src/components/browser/browser.tsx
@@ -1,9 +1,27 @@
-import BaseBrowser from "./BaseBrowser";
+import { useMemo } from "react";
 import { builtInTrackDefinitions } from "../tracks/builtins";
 import { TrackDefinition } from "../tracks/types";
 import { BrowserStoreInstance } from "../../store/browserStore";
 import { TrackStoreInstance } from "../../store/trackStore";
-import { DataStoreInstance } from "../../store/dataStore";
+import { createDataStoreMemo, DataStoreInstance } from "../../store/dataStore";
+import { createContextMenuStore } from "../../store/contextMenuStore";
+import { createModalStore } from "../../store/modalStore";
+import { createTooltipStore } from "../../store/tooltipStore";
+import { createThemeStore } from "../../store/themeStore";
+import { BrowserProvider, useBrowserStore, useTheme, useTrackStore } from "../../store/BrowserContext";
+import { useDataFetcher } from "../../hooks/useDataFetcher";
+import ContextMenu from "../contextMenu/contextMenu";
+import Modal from "../modal/modal";
+import Tooltip from "../tooltip/tooltip";
+import Ruler from "../tracks/ruler/ruler";
+import DragTrack from "../tracks/frame/DragTrack";
+import Margin from "../tracks/frame/Margin";
+import TrackFrame from "../tracks/frame/TrackFrame";
+import SVGWrapper from "./svgWrapper";
+import SelectRegion from "../tracks/ruler/selectRegion";
+import Highlights from "../highlight/highlights";
+import { RULER_HEIGHT } from "../tracks/ruler/ruler";
+import { createTrackRegistry, getDefinition } from "../tracks/registry";
 
 interface BrowserProps {
   browserStore: BrowserStoreInstance;
@@ -12,6 +30,81 @@ interface BrowserProps {
   customTracks?: TrackDefinition[];
 }
 
-export default function Browser({ customTracks = [], ...props }: BrowserProps) {
-  return <BaseBrowser {...props} trackDefinitions={[...builtInTrackDefinitions, ...customTracks]} />;
+function BrowserContent() {
+  const trackIds = useTrackStore((state) => state.ids);
+
+  useDataFetcher();
+
+  return (
+    <>
+      <SVGWrapper>
+        <SelectRegion />
+        <RulerFrame />
+        <Highlights />
+        {trackIds.map((id) => (
+          <TrackFrame key={id} id={id} />
+        ))}
+        <Tooltip />
+      </SVGWrapper>
+      <ContextMenu />
+      <Modal />
+    </>
+  );
+}
+
+function RulerFrame() {
+  const background = useTheme((state) => state.background);
+  const browserWidth = useBrowserStore((state) => state.browserWidth);
+  const marginWidth = useBrowserStore((state) => state.marginWidth);
+
+  return (
+    <g id="ruler-frame">
+      <DragTrack id="ruler">
+        <g transform={`translate(${marginWidth},0)`}>
+          <Ruler />
+        </g>
+      </DragTrack>
+      <Margin
+        id="ruler"
+        height={RULER_HEIGHT}
+        color={background}
+        swapping={false}
+        verticalMargin={0}
+        onHover={() => {}}
+        onLeave={() => {}}
+      />
+      <line stroke="#ccc" x1={0} x2={browserWidth} y1={RULER_HEIGHT} y2={RULER_HEIGHT} />
+    </g>
+  );
+}
+
+export default function Browser({ browserStore, trackStore, externalDataStore, customTracks = [] }: BrowserProps) {
+  const dataStore = externalDataStore ?? createDataStoreMemo([]);
+  const contextMenuStore = useMemo(() => createContextMenuStore(), []);
+  const modalStore = useMemo(() => createModalStore(), []);
+  const tooltipStore = useMemo(() => createTooltipStore(), []);
+  const themeStore = useMemo(() => createThemeStore(), []);
+  const trackDefinitions = useMemo(() => [...builtInTrackDefinitions, ...customTracks], [customTracks]);
+  const trackRegistry = useMemo(() => createTrackRegistry(trackDefinitions), [trackDefinitions]);
+  const getTrackDefinition = useMemo(() => (type: string) => getDefinition(trackRegistry, type), [trackRegistry]);
+
+  const contextValue = useMemo(
+    () => ({
+      browserStore,
+      trackStore,
+      dataStore,
+      contextMenuStore,
+      modalStore,
+      tooltipStore,
+      themeStore,
+      getTrackDefinition,
+    }),
+    [browserStore, trackStore, dataStore, contextMenuStore, modalStore, tooltipStore, themeStore, getTrackDefinition]
+  );
+
+  return (
+    <BrowserProvider value={contextValue}>
+      <BrowserContent />
+    </BrowserProvider>
+  );
 }
