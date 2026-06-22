0e8909842cd621e1dcdfa6701b1718a67e419f6f
Jair Meza <jair3202@gmail.com>
2026-06-22 19:01:48 -0400

    mukamel tracks

--- packages/v2/test/app/App.tsx
+++ packages/v2/test/app/App.tsx
@@ -1,5 +1,4 @@
 import {
-  GenomeBrowser,
   createBrowserStore,
   bigBedModule,
   bulkBedModule,
@@ -8,6 +7,13 @@ import {
   methylCModule,
   transcriptModule,
 } from "../../src/lib";
+import { GenomeBrowser } from "../../src_new/lib";
+import {
+  bigBedExample,
+  bigWigExample,
+  MukamelMethylC,
+  transcriptExample,
+} from "./tracks";
 
 const browserStore = createBrowserStore({
   region: "chr12:53,372,922-53,423,700",
@@ -32,112 +38,11 @@ const modules = [
 const trackStore = createTrackStore({
   modules,
   tracks: [
-    bigWigModule.create({
-      id: "dnase",
-      title: "DNase aggregate",
-      color: "#1B2021",
-      height: 50,
-      url: "https://downloads.wenglab.org/DNAse_All_ENCODE_MAR20_2024_merged.bw",
-    }),
-    bigBedModule.create({
-      id: "astro-peaks",
-      title: "Astro peaks",
-      color: "#4b9560",
-      display: "squish",
-      height: 35,
-      url: "https://downloads.wenglab.org/Astro.PeakCalls.bb",
-      onClick: ({ item }) => {
-        console.log("[v2 test app] BigBed click", {
-          name: item.name,
-          start: item.start,
-          end: item.end,
-          item,
-        });
-      },
-    }),
-    bulkBedModule.create({
-      id: "chip-bulk-peaks",
-      title: "bulk BigBed",
-      color: "#db6d28",
-      height: 30,
-      gap: 2,
-      datasets: [
-        {
-          name: "ChIP Dataset 1",
-          url: "https://downloads.wenglab.org/ChIP_ENCSR000AKA-ENCSR000AKC-ENCSR000AKF-ENCSR000AKE-ENCSR000AKD-ENCSR000AOX.bigBed",
-        },
-        {
-          name: "ChIP Dataset 2",
-          url: "https://downloads.wenglab.org/ChIP_ENCSR000EWA-ENCSR000AKP-ENCSR000EWC-ENCSR000DWB-ENCSR000EWB-ENCSR000APE.bigBed",
-        },
-        {
-          name: "ChIP Dataset 3",
-          url: "https://downloads.wenglab.org/ChIP_ENCSR000ARA-ENCSR000AQW-ENCSR000AQY-ENCSR000AQX-ENCSR000ASX-ENCSR000ARZ.bigBed",
-        },
-      ],
-      onClick: ({ item }) => {
-        console.log("[v2 test app] BulkBed click", {
-          name: item.name,
-        });
-      },
-    }),
-    transcriptModule.create({
-      id: "genes",
-      title: "Genes",
-      assembly: "GRCh38",
-      version: 40,
-      geneName: "SOX4",
-      display: "pack",
-      color: "#7a4fb3",
-      canonicalColor: "#d45c2f",
-      highlightColor: "#1f77b4",
-      height: 35,
-      onClick: ({ item }) => {
-        console.log(item.name);
-      },
-    }),
-    methylCModule.create({
-      id: "methylc",
-      title: "MethylC Track",
-      height: 100,
-      colors: {
-        cpg: "#648bd8",
-        chg: "#ff944d",
-        chh: "#ff00ff",
-        depth: "#525252",
-      },
-      range: { min: 0, max: 1 },
-      urls: {
-        plusStrand: {
-          cpg: {
-            url: "https://users.wenglab.org/mezaj/mohd/EB100001/EB100001_cpg_pos.bw",
-          },
-          chg: {
-            url: "https://users.wenglab.org/mezaj/mohd/EB100001/EB100001_chg_pos.bw",
-          },
-          chh: {
-            url: "https://users.wenglab.org/mezaj/mohd/EB100001/EB100001_chh_pos.bw",
-          },
-          depth: {
-            url: "https://users.wenglab.org/mezaj/mohd/EB100001/EB100001_coverage_pos.bw",
-          },
-        },
-        minusStrand: {
-          cpg: {
-            url: "https://users.wenglab.org/mezaj/mohd/EB100001/EB100001_cpg_neg.bw",
-          },
-          chg: {
-            url: "https://users.wenglab.org/mezaj/mohd/EB100001/EB100001_chg_neg.bw",
-          },
-          chh: {
-            url: "https://users.wenglab.org/mezaj/mohd/EB100001/EB100001_chh_neg.bw",
-          },
-          depth: {
-            url: "https://users.wenglab.org/mezaj/mohd/EB100001/EB100001_coverage_neg.bw",
-          },
-        },
-      },
-    }),
+    bigWigExample,
+    bigBedExample,
+    transcriptExample,
+    MukamelMethylC("CGE_ADARB2_ADAM33"),
+    MukamelMethylC("MGE_SST_RAB31.young"),
   ],
 });
 
