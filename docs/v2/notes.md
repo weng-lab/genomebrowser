48d3adb53a28b128e8e84aac2819e8aa7bd87835
Jair Meza <jair3202@gmail.com>
2026-06-30 01:50:15 -0400

    Revert "add ultracite"
    This reverts commit 045b94e94d2c1514a794b8f31b7c84f75eb1f5fb.

--- docs/v2/notes.md
+++ docs/v2/notes.md
@@ -1,6 +1,2 @@
 Create docs for high level usage of the browser
 then create tips and tricks/cookbook style docs for customizing tracks
-
-## Ideas
-
-An npm cli that accepts a json input to set internal tracks in a local browser Next or SPA app
--- oxlint.config.ts
+++
@@ -1,8 +0,0 @@
-import { defineConfig } from "oxlint";
-import core from "ultracite/oxlint/core";
-import react from "ultracite/oxlint/react";
-
-export default defineConfig({
-  extends: [core, react],
-  ignorePatterns: core.ignorePatterns,
-});
